a8952b9b3659503c2ae97427d30cef893f67d9c2
Evan Cole <evancole@usc.edu>
2023-02-27 02:37:26 +0000

    final commit

--- logicsim/circuit.cpp
+++ logicsim/circuit.cpp
@@ -13,7 +13,8 @@
 
 Circuit::Circuit() : m_current_time(0)
 {
-    
+    EventLess e;
+    Heap<Event*, EventLess>m_pq(2, e);
 }
 
 Circuit::~Circuit()
@@ -110,6 +111,14 @@ bool Circuit::parse(const char* fname)
                     m_gates.push_back(new Or2Gate(m_wires[stoi(s_in1)], m_wires[stoi(s_in2)], m_wires[stoi(s_output)]));
                 }
                 //Add code here to support the NOT gate type
+                if(s_type == "NOT")
+                {
+                    std::string s_in1;
+                    getline(ss, s_in1, ',');
+                    std::string s_output;
+                    getline(ss, s_output, ',');
+                    m_gates.push_back(new NotGate(m_wires[stoi(s_in1)], m_wires[stoi(s_output)]));
+                }
             }
         }
         if(line == "INJECT")
